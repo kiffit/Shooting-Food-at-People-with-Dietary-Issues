a38ae527d343410d081938a6700bb2843f397644
Braxton Rider <126611124+SirRexOfRider@users.noreply.github.com>
2025-04-23 12:08:56 -0500

    Update driver.py

--- Code/driver.py
+++ Code/driver.py
@@ -5,9 +5,12 @@
 from Utilities.BoundingBox import BoundingBox
 from pygame.math import Vector2
 from Gui.GameWindow import GameWindow
+from Gui.GameController import GameController
 
 
 def main():
+    gc = GameController()
+    print(gc)
     gw1 = GameWindow()
     gw1.run()
 
